8404505a1887e9843d1906bcdfb2436b6a10b4f2
Gareth <sebastianmv2019@gmail.com>
2025-06-23 13:04:23 -0500

    build: add primevue ui dependency

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Nora from '@primeuix/themes/nora'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+    theme: {
+        preset: Nora
+    }
+})
 app.use(createPinia())
 app.use(router)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <script setup>
+import Button from "primevue/button"
 </script>
 
 <template>
     <h1>Oña</h1>
+    <Button label="verify"/>
+    
 </template>
